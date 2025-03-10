41e12da598ed05fd9c44f5c893db48f99ca2a8c1
Fathima sahla <sahlarazak234@gmail.com>
2025-03-10 15:29:50 +0400

    added backend url to script file

--- Server/script.js
+++ Server/script.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://e-commerce-app-nu-sage.vercel.app/",
     credentials: true
   }))
 
